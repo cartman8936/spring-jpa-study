1c1892af3a8562d1b17cdec19fc6b56d06378c9d
cartman8936 <hehahehe@naver.com>
2020-05-23 17:15:44 +0900

    예제 Entity 설계

--- jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
+++ jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jpashop2Application {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("hello");
-		System.out.println(hello.getData());
+
 		SpringApplication.run(Jpashop2Application.class, args);
 	}
 
